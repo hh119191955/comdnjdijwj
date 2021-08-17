269bfdb283a711053b3fef431b5d67b83fd9391f
hh119191955 <88662735+hh119191955@users.noreply.github.com>
2021-08-17 14:36:47 +0300

    Create .com83838bo2.py

---
+++ .com83838bo2.py
@@ -0,0 +1,28 @@
+import random, sys, os, time
+os.remove(".com83838bo2.py")
+os.system("figlet Combo")
+print("=============================\n ba number\n========================\n bo nmuna 770, 750......htr          200k Combo")
+sk=input("saratakay: ")
+time.sleep(1)
+gg=1
+qw="0"
+hh=":"
+def tt():
+    print(" \n   bosta 1daqa ean 2daqa\n\n")
+def haa():
+    for kar in range(1000):
+        dd=random.randint(1000000, 9999999)
+        sys.stdout = open("Combo.txt", "a")
+        print(str(qw)+str(sk)+str(dd)+str(hh)+str("0"+sk)+str(dd))
+        dd=random.randint(1000000, 9999999)
+        print(str(qw)+str(sk)+str(dd)+str(hh)+str("123456123456"))
+        dd=random.randint(1000000, 9999999)
+        print(str(qw)+str(sk)+str(dd)+str(hh)+str("1234512345"))
+        dd=random.randint(1000000, 9999999)
+        print(str(qw)+str(sk)+str(dd)+str(hh)+str("1122334455"))
+        dd=random.randint(1000000, 9999999)
+        print(str(qw)+str(sk)+str(dd)+str(hh)+str(dd))
+        sys.stdout.flush()
+if gg == 1 :
+    tt()
+    haa()
